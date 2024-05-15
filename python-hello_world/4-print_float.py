e2621f128dad6ffc9fd94f52d28f7130d2df49dd
jamesbondie <jamesalfreddie@gmail.com>
2024-05-15 23:40:37 +0400

    ValueError added

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f'Float: {"%.2f" % number}')
+try:
+    print(f'Float: {"%.2f" % number}')
+except ValueError:
+    print("Unknown format code 'f' for object of type 'str'")
