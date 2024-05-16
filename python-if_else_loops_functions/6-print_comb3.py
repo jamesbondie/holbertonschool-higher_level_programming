260cb44c5854dc19e62885c6f6bbbb125efda067
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 20:37:59 +0400

    space position corrected

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,9 @@
 
 for i in range(0, 10):
     if (i // 10 != i % 10):
-        print("{:02d} ,".format(i), end="")
+        print("{:02d}, ".format(i), end="")
 
 for i in range(10, 88):
     if (i // 10 < i % 10):
-        print("{:02d} ,".format(i), end="")
+        print("{:02d}, ".format(i), end="")
 print(89)
