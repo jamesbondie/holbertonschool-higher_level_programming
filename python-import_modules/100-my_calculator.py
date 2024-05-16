78559ae7922577b54cd8f8886cce9c21d718c034
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 19:25:39 +0400

    variable declared, pycodestyle corrected, chmod is given

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,18 +2,23 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-if (len(sys.argv) - 1 != 3):
-  print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
-  exit(1)
-else:
-  if sys.argv[2] == "+":
-    print(f"{sys.argv[1]} + {sys.argv[3]} = {add(int(sys.argv[1]), int(sys.argv[3]))}")
-  elif sys.argv[2] == "-":
-    print(f"{sys.argv[1]} - {sys.argv[3]} = {sub(int(sys.argv[1]), int(sys.argv[3]))}")
-  elif sys.argv[2] == "*":
-    print(f"{sys.argv[1]} * {sys.argv[3]} = {mul(int(sys.argv[1]), int(sys.argv[3]))}")
-  elif sys.argv[2] == "/":
-    print(f"{sys.argv[1]} / {sys.argv[3]} = {div(int(sys.argv[1]), int(sys.argv[3]))}")
-  else:
-    print("Unknown operator. Available operators: +, -, * and /\n")
-    exit(1)
+if __name__ == '__main__':
+    a == sys.argv[1]
+    b == sys.argv[2]
+    c == sys.argv[3]
+    d == sys.argv[4]
+    if (len(sys.argv) - 1 != 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        exit(1)
+    else:
+        if sys.argv[2] == "+":
+            print(f"{a} + {c} = {add(int(a), int(sys.argv[3]))}")
+        elif sys.argv[2] == "-":
+            print(f"{a} - {c} = {sub(int(a), int(sys.argv[3]))}")
+        elif sys.argv[2] == "*":
+            print(f"{a} * {c} = {mul(int(a), int(sys.argv[3]))}")
+        elif sys.argv[2] == "/":
+            print(f"{a} / {c} = {div(int(a), int(sys.argv[3]))}")
+        else:
+            print("Unknown operator. Available operators: +, -, * and /\n")
+            exit(1)
