0b71b13088dc770ab949fbd46ab4976e2759b59d
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 22:46:58 +0400

    indendation corrected

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-
 def pow(a, b):
     i = 0
     c = a
     if b > 0:
         i = 0
-    while (i < b - 1):
-        a *= c
-        i += 1
-    return a
+        while (i < b - 1):
+            a *= c
+            i += 1
+        return a
     elif b == 0:
         return 1
     else:
