9f74c62eebbfdb7420e693a5a721fd55263c8047
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 19:44:12 +0400

    parenthesis closed and condition changed for out of range

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,15 +3,15 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == '__main__':
-    a = argv[1]
-    b = argv[2]
-    c = argv[3]
     if (len(argv) - 1 != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         exit(1)
     else:
+        a = argv[1]
+        b = argv[2]
+        c = argv[3]
         if b == "+":
-            print(f"{a} + {c} = {add(int(a), int(c)}")
+            print(f"{a} + {c} = {add(int(a), int(c))}")
         elif b == "-":
             print(f"{a} - {c} = {sub(int(a), int(c))}")
         elif b == "*":
