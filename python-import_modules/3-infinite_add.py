daffaa63c20ad1e4500978a9f237371c98396eff
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 18:40:31 +0400

    chmod, pycodestyle, main conditions corrected

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
-
-i = 1
-result = 0
-while (i < len(sys.argv)):
-    result += int(sys.argv[i])
-    i += 1
-print(result)
+if __name__ == '__main__':
+    i = 1
+    result = 0
+    while (i < len(sys.argv)):
+        result += int(sys.argv[i])
+        i += 1
+    print(result)
