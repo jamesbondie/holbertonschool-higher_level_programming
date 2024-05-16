140012fe9a90cc4a471ab742b48bdf046490a051
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 18:24:53 +0400

    pycodestyle corrected

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     a = 1
     b = 2
     if (b >= 0):
-        print ("{0} + {1} = {2}".format(a, b, add(a, b)))
+        print("{0} + {1} = {2}".format(a, b, add(a, b)))
     else:
-        print ("{0} + {1} = {2}".format(a, b, a - b))
+        print("{0} + {1} = {2}".format(a, b, a - b))
