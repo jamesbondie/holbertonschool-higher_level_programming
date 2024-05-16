123dd4b1cca979568d1ff94da77cbeae55302bcc
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 18:36:30 +0400

    only 1 argument is argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     if (len(sys.argv) - 1 == 0):
         print(f"{len(sys.argv) - 1} arguments.")
     else:
-        if (len(sys.argv) - 1) % 2 == 0:
-            print(f"{len(sys.argv) - 1} arguments:")
-        else:
+        if len(sys.argv) - 1 == 1:
             print(f"{len(sys.argv) - 1} argument:")
+        else:
+            print(f"{len(sys.argv) - 1} arguments:")
 
     i = 1
     while (i < len(sys.argv)):
