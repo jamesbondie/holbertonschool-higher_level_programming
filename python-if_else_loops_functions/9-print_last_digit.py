11ccf7736f37e7e9d80715cc3c892a5a00cbfa42
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 22:39:34 +0400

    condition added, return changed for condition

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    print("{}".format(number % 10))
-    return number % 10
+    
+    if (number < 0):
+        print("{}".format(10 - number % 10), end="")
+        return (10 - number % 10)
+    else:
+        print("{}".format(number%10), end="")
+        return (number % 10)
