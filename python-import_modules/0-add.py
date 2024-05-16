32c999f17cae2a086aae67f8f8c4b8230fd82338
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 18:27:16 +0400

    condition deleted because of permission

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,7 +3,4 @@ if __name__ == '__main__':
     from add_0 import add
     a = 1
     b = 2
-    if (b >= 0):
-        print("{0} + {1} = {2}".format(a, b, add(a, b)))
-    else:
-        print("{0} + {1} = {2}".format(a, b, a - b))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
