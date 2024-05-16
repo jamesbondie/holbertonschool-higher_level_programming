9c670a2277794e8bdf8b6711883af30e3c32b27d
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 19:39:34 +0400

    all sys argvs are now argv and d variable deleted

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,19 +6,18 @@ if __name__ == '__main__':
     a = argv[1]
     b = argv[2]
     c = argv[3]
-    d = argv[4]
     if (len(argv) - 1 != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         exit(1)
     else:
-        if argv[2] == "+":
-            print(f"{a} + {c} = {add(int(a), int(sys.argv[3]))}")
-        elif argv[2] == "-":
-            print(f"{a} - {c} = {sub(int(a), int(sys.argv[3]))}")
-        elif argv[2] == "*":
-            print(f"{a} * {c} = {mul(int(a), int(argv[3]))}")
-        elif argv[2] == "/":
-            print(f"{a} / {c} = {div(int(a), int(argv[3]))}")
+        if b == "+":
+            print(f"{a} + {c} = {add(int(a), int(c)}")
+        elif b == "-":
+            print(f"{a} - {c} = {sub(int(a), int(c))}")
+        elif b == "*":
+            print(f"{a} * {c} = {mul(int(a), int(c))}")
+        elif b == "/":
+            print(f"{a} / {c} = {div(int(a), int(c))}")
         else:
             print("Unknown operator. Available operators: +, -, * and /\n")
             exit(1)
