a48ac671a1ab00c7fe7e5bc4e6c0d6966046c849
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 21:11:44 +0400

    format changed

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     str2 = ''
     for i in str:
         if (ord(i) < 123 and ord(i) > 96):
-            str2 = str2 + chr(ord(i)-32)
+            str2 = str2 + chr(ord(i) - 32)
         else:
             str2 = str2 + i
-    print(f'{str2}')
+    print('{}'.format(str))
