eee514af7bb1a026aead999f67a37082e8881134
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 22:59:27 +0400

    number:.2f

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-try:
-    print(f'Float: {"%.2f" % number}')
-except ValueError:
-    print("Unknown format code 'f' for object of type 'str'")
+print(f'Float: {number:.2f}')
