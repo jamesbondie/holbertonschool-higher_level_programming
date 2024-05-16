33d5078c2abaf2cbb3ab4976aaebba050e9e912d
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 19:37:14 +0400

    sys argv is now argv

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,22 +3,22 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == '__main__':
-    a = sys.argv[1]
-    b = sys.argv[2]
-    c = sys.argv[3]
-    d = sys.argv[4]
-    if (len(sys.argv) - 1 != 3):
+    a = argv[1]
+    b = argv[2]
+    c = argv[3]
+    d = argv[4]
+    if (len(argv) - 1 != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         exit(1)
     else:
-        if sys.argv[2] == "+":
+        if argv[2] == "+":
             print(f"{a} + {c} = {add(int(a), int(sys.argv[3]))}")
-        elif sys.argv[2] == "-":
+        elif argv[2] == "-":
             print(f"{a} - {c} = {sub(int(a), int(sys.argv[3]))}")
-        elif sys.argv[2] == "*":
-            print(f"{a} * {c} = {mul(int(a), int(sys.argv[3]))}")
-        elif sys.argv[2] == "/":
-            print(f"{a} / {c} = {div(int(a), int(sys.argv[3]))}")
+        elif argv[2] == "*":
+            print(f"{a} * {c} = {mul(int(a), int(argv[3]))}")
+        elif argv[2] == "/":
+            print(f"{a} / {c} = {div(int(a), int(argv[3]))}")
         else:
             print("Unknown operator. Available operators: +, -, * and /\n")
             exit(1)
