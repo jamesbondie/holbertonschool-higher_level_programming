c6fdfbec013ba59af3457218aff3303c1a3f6851
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 19:33:14 +0400

    equation corrected

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,10 +3,10 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == '__main__':
-    a == sys.argv[1]
-    b == sys.argv[2]
-    c == sys.argv[3]
-    d == sys.argv[4]
+    a = sys.argv[1]
+    b = sys.argv[2]
+    c = sys.argv[3]
+    d = sys.argv[4]
     if (len(sys.argv) - 1 != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         exit(1)
