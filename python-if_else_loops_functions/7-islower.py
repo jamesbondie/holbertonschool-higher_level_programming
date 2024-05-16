21fbb9fd2056923cf036e9c53c630ef26c0ce521
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 21:08:52 +0400

    condition added for char

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def islower(c):
-    for i in range(97, 123):
-        if (c == chr(i)):
-            return True
-        else:
-            return False
-
+    if c >= 97 and c <= 123:
+        for i in range(97, 123):
+            if (c == chr(i)):
+                return True
+            else:
+                return False
