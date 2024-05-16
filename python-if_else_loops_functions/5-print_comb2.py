e68e7c7a579d537b0e9cf7769dcfca0ce19b913a
jamesbondie <jamesalfreddie@gmail.com>
2024-05-16 20:27:05 +0400

    different format for i, 02

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0,99):
-    print(f'{i:02d}, ', end="")
-print(f'{99:02d}')
+for i in range(0, 99):
+    print("{}, ".format(i, '02'), end="")
+print('{}'.format(99, '02'))
