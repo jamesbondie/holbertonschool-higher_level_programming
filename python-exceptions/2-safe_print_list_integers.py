6a4d56a5f7305bcd3a97da639c5c4e16c9c768c9
jamesbondie <jamesalfreddie@gmail.com>
2024-05-25 19:19:53 +0400

    condition changed for :d and type value errors handled

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     lenn = 0
     for i in range(x):
-        if type(my_list[i]) is int:
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                lenn += 1
-            except:
-                print(end="")
-        else:
-            print(end="")
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            lenn += 1
+        except (ValueError, TypeError):
+            continue
+    print()
     return lenn
