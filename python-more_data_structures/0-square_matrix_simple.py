9d29cff7c3bbbead2ad84cf16f12e44e5dc7719e
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 16:38:49 +0400

    deepcopy removed and new matrix created for each row with for loop

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-import copy
 def square_matrix_simple(matrix=[]):
-    new_matrix = copy.deepcopy(matrix)
-    i, j, k = 0, 0, 0
-    while (i < len(matrix)):
-        k = 0
-        for j in matrix[i]:
-            new_matrix[i][k] = j**2
-            k += 1
-        i += 1
+    new_matrix = []
+    for i in matrix:
+        k = [x**2 for x in i]
+        new_matrix.append(k)
     return new_matrix
