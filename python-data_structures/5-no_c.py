36e96cecfac9ec351f78ba50303c7cfdf18eb865
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 18:47:29 +0400

    structure has been changed

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,8 +3,8 @@
 def no_c(my_string):
     i = 0
     my_str = ''
-    while (i < len(my_string) - 1):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            my_str = my_string[0:i] + my_string[(i + 1):]
+    while (i < len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            my_str += my_string[i]
         i += 1
     return my_str
