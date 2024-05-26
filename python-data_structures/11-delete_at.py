948f383d877ff49e76b1dff917502fd29e1ee186
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 19:02:50 +0400

    len not len-1

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@ def delete_at(my_list=[], idx = 0):
     i = 0
     if (idx > len(my_list) - 1 or idx < 0):
         return my_list
-    while (i < len(my_list) - 1):
+    while (i < len(my_list)):
         if (i == idx):
             del my_list[idx]
             return my_list
