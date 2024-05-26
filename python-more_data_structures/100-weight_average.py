7883115c9ecfd5f40156d538473cc5911c1214eb
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 16:26:10 +0400

    files has been created

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    summy = 0
+    divider = 0
+    x = 0
+
+    while (x < len(my_list)):
+        summy += my_list[x][0] * my_list[x][1]
+        divider += my_list[x][1]
+        x += 1
+    return summy/divider
