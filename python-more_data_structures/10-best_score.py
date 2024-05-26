2249d6669c98a5e73ec59492694f4ae53fa16f0c
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 17:07:50 +0400

    condition changed for None and {}x

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if len(a_dictionary) == 0:
+        return None
     sorted_dict = dict(sorted(a_dictionary.items(), key=lambda i: i[1]))
     sorted_dict_list = list(sorted_dict)
     return sorted_dict_list[-1]
