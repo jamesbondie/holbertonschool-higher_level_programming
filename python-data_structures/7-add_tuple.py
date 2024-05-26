d2efd4f6780005bb431ee41a848be916b4189897
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 19:50:34 +0400

    format changed

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = ''
-    i = 0
-    if (len(tuple_a) > len(tuple_b):
-        for i in tuple_b:
-               new_tuple = ([tuple_a[i] + tuple_b[i]
+    if len(tuple_a) > 0:
+        a_0 = tuple_a[0]
+    else:
+        a_0 = 0
+    if len(tuple_a) > 1:
+        a_1 = tuple_a[1]
+    else:
+        a_1 = 0
+    if len(tuple_b) > 0:
+        b_0 = tuple_b[0]
+    else:
+        b_0 = 0
+    
+    if len(tuple_b) > 1:
+        b_1 = tuple_b[1]
+    else:
+        b_1 = 0
+    
+    new_tuple = (a_0 + b_0, a_1 + b_1)
+    return new_tuple
