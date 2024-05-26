fac0b6bf7511496884c612737d1788d9eace147e
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 16:31:12 +0400

    condition added for zero division

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,7 +4,8 @@ def weight_average(my_list=[]):
     summy = 0
     divider = 0
     x = 0
-
+    if len(my_list) == 0:
+        return 0
     while (x < len(my_list)):
         summy += my_list[x][0] * my_list[x][1]
         divider += my_list[x][1]
