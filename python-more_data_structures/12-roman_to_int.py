6be398334e0b874021ccaa2c263e94fe86c02321
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 17:16:15 +0400

    not string and none conditions added

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    if type(roman_string) is not str:
+        return 0
     rdic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     k, summy = 0, 0
     lst = list(roman_string)
