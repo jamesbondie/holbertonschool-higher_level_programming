661e020bcc8e65943b6ee1dc29558f9fb47d4614
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 18:04:07 +0400

    type is used in :d

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if type(value) is int:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except:
         return False
