a1a48812ea28da6b9e68d450522724e567a5e6cd
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 17:05:15 +0400

    pycode corrected, condition changed

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if len(a_dictionary) == 0:
         return None
-    sorted_dict = dict(sorted(a_dictionary.items(), key = lambda item: item[1]))
+    sorted_dict = dict(sorted(a_dictionary.items(), key=lambda i: i[1]))
     sorted_dict_list = list(sorted_dict)
     return sorted_dict_list[-1]
