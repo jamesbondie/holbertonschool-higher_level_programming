e37d8097626fa724e1039e78232d1a6246b7beb0
jamesbondie <jamesalfreddie@gmail.com>
2024-05-26 16:58:13 +0400

    sorting changed not for alphabetically, by numerically

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    sorted_dict = sorted(a_dictionary)
+    sorted_dict = dict(sorted(a_dictionary.items(), key = lambda item: item[1]))
     sorted_dict_list = list(sorted_dict)
     return sorted_dict_list[-1]
