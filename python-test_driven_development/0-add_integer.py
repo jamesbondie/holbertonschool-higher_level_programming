79ef69dae982a3d32648af7baaf0c5164e18f673
jamesbondie <jamesalfreddie@gmail.com>
2024-05-30 16:51:46 +0400

    overflow condition added

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,13 @@
 
 def add_integer(a, b=98):
     """This is our integer adder function"""
+    
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+    if a + 1 == a or b + 1 == b:
+        raise OverflowError
     if type(a) is float:
         a = int(a)
     if isinstance(b, float):
