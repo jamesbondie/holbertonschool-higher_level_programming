e6d9634b7b0e35c2a7010e05e7863baa1ff9891e
jamesbondie <jamesalfreddie@gmail.com>
2024-05-31 13:36:15 +0400

    text indentation updated v2

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,8 +6,7 @@ def text_indentation(text):
     """This is our function"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    y = text.replace(" \\", "")
-    listr = [". ", ": ", "? "]
-    for i in listr:
-        y = y.replace(i, i[0:1] + "\n\n")
-    print(y)
+    for i in [". ", ": ", "? "]:
+        text = text.replace(" \\", "").replace(i, i[0:1] + "\n\n")
+    for i in text:
+        print(i, end="")
