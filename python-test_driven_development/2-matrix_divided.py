260aa024df0d888e33b1264a7b8be50dfbb4127a
jamesbondie <jamesalfreddie@gmail.com>
2024-05-31 11:26:27 +0400

    pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,10 +2,11 @@
 """This is our module"""
 def matrix_divided(matrix, div):
     """This is our function"""
+    typerror = "matrix must be a matrix (list of lists) of integers/floats"
     for k in matrix:
         for i in k:
             if not isinstance(i, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(typerror)
     for i in range(len(matrix)):
         if len(matrix[i]) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
@@ -13,6 +14,6 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = list(map(lambda x: list(map(lambda z: z/div, x)), matrix))
-    rounded_new_matrix = list(map(lambda x: list(map(lambda c: round(c, 2), x)), new_matrix))
-    return rounded_new_matrix
+    new = list(map(lambda x: list(map(lambda z: z/div, x)), matrix))
+    roun = list(map(lambda x: list(map(lambda c: round(c, 2), x)), new))
+    return roun
