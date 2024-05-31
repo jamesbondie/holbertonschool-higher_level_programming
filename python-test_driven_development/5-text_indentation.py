dc97ffa8ede3bd851aa3a7f033c56ede18ae4df1
jamesbondie <jamesalfreddie@gmail.com>
2024-05-31 15:24:03 +0400

    split and strip saved this function

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,9 +6,7 @@ def text_indentation(text):
     """This is function"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i in [". ", ": ", "? "]:
-        text = text.replace(i, i[0])
-    for i in text:
-        print(i, end="")
-        if i == "." or i == ":" or i == "?":
-            print('\n\n', end="")
+    for i in [".", "?", ":"]:
+        text = text.replace(i, i + '\n\n')
+    for i in text.split('\n'):
+        print(i.strip())
