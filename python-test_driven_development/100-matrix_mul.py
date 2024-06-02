d2f5f34009161a40a478023c1f61841408371db4
jamesbondie <jamesalfreddie@gmail.com>
2024-06-02 19:31:30 +0400

    numpy added

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python3
-"""This module is for multiplication of matrices"""
+"""IS DOCUMENTED"""
 
 
 def matrix_mul(m_a, m_b):
-    """This function multiplies 2 matrices"""
+    """IS DOCUMENTED"""
+
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    
-
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    
 
-    for i in range(len(m_a)):
-        if not isinstance(m_a[i], list):
+    for row in m_a:
+        if not isinstance(row, list):
             raise TypeError("m_a must be a list of lists")
-        if not isinstance(m_a[i], (int, float)):
-            raise TypeError("m_a should contain only integers or floats")
-        if len(m_a[i]) != len(m_a[0]):
-            raise TypeError("each row of m_a must be of the same size")
-    
-
-    for i in range(len(m_b)):
-        if not isinstance(m_b[i], list):
+    for row in m_b:
+        if not isinstance(row, list):
             raise TypeError("m_b must be a list of lists")
-        if not isinstance(m_b[i], (int, float)):
-            raise TypeError("m_b should contain only integers or floats")
-        if len(m_b[i]) != len(m_b[0]):
-            raise TypeError("each row of m_b must be of the same size")
 
-
-    if len(m_a) == 0:
+    if not any(m_a):
         raise ValueError("m_a can't be empty")
-    if len(m_b) == 0:
+    if not any(m_b):
         raise ValueError("m_b can't be empty")
-    
+
+    for row in m_a:
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+
+    for row in m_a:
+        if len(row) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")
+    for row in m_b:
+        if len(row) != len(m_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    for i in m_a:
 
+    rows_a, cols_a = len(m_a), len(m_a[0])
+    rows_b, cols_b = len(m_b), len(m_b[0])
+    result = [[0 for _ in range(cols_b)] for _ in range(rows_a)]
+    for i in range(rows_a):
+        for j in range(cols_b):
+            for k in range(cols_a):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+    return result
--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,4 +1,45 @@
 #!/usr/bin/python3
+"""IS DOCUMENTED"""
+import numpy as np
 
-def lazy_matrix_mul(m_a, m_b):
+def matrix_mul(m_a, m_b):
+    """IS DOCUMENTED"""
 
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    for row in m_a:
+        if not isinstance(row, list):
+            raise TypeError("m_a must be a list of lists")
+    for row in m_b:
+        if not isinstance(row, list):
+            raise TypeError("m_b must be a list of lists")
+
+    if not any(m_a):
+        raise ValueError("m_a can't be empty")
+    if not any(m_b):
+        raise ValueError("m_b can't be empty")
+
+    for row in m_a:
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+
+    for row in m_a:
+        if len(row) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")
+    for row in m_b:
+        if len(row) != len(m_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = np.dot(np.array(m_a), np.array(m_b))
+    return result
