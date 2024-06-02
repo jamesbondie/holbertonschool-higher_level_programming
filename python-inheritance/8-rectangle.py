07a37419fc8445c5188b23be484d83a32ed854d1
jamesbondie <jamesalfreddie@gmail.com>
2024-06-02 15:10:46 +0400

    issubclass corrected

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -19,5 +19,5 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = 0
         self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
