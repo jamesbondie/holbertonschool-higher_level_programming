a23c76ddefafed2d6c3507809632a7a565d467fa
jamesbondie <jamesalfreddie@gmail.com>
2024-06-02 15:27:34 +0400

    changed to super

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,11 +2,10 @@
 """IS DOCUMENTED"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """IS DOCUMENTED"""
     def __init__(self, size):
-        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return super().area()
+        super().integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
