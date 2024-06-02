428715945862bb2917808d7bad0fdde9e84b1383
jamesbondie <jamesalfreddie@gmail.com>
2024-06-02 19:51:29 +0400

    scalar is not allowed

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -41,5 +41,4 @@ def lazy_matrix_mul(m_a, m_b):
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    result = np.dot(np.array(m_a), np.array(m_b))
-    return result
+    return np.matmul(m_a, m_b)
