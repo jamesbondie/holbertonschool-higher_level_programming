04d94eda0e42fa52f580e2bbc5004b6befe93667
jamesbondie <jamesalfreddie@gmail.com>
2024-06-02 15:19:14 +0400

    import changed

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,34 +1,6 @@
 #!/usr/bin/python3
 """IS DOCUMENTED"""
-
-
-class BaseGeometry:
-    """IS DOCUMENTED"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """IS DOCUMENTED"""
-    def __init__(self, width, height):
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
