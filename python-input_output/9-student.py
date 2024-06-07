8a492134007a5fd008a1b03c7acace2c143586e8
jamesbondie <jamesalfreddie@gmail.com>
2024-06-07 17:57:24 +0400

    last name corrected;

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -9,7 +9,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
-        self.last_name = last.name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
