23f3041712971ea6d93d584671769379105c854f
jamesbondie <jamesalfreddie@gmail.com>
2024-06-09 01:02:30 +0400

    with one element

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 lisst = []
 try:
     load_from_json_file("add_item.json")
-    i = 1
-    while i < len(sys.argv):
-        lisst.append(sys.argv[i])
-        i += 1
 except Exception:
     pass
+i = 1
+while i < len(sys.argv):
+    lisst.append(sys.argv[i])
+    i += 1
 save_to_json_file(lisst, "add_item.json")
