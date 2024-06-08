9492a0f6514d32a9841c2a7a8ec8515bbfe764ec
jamesbondie <jamesalfreddie@gmail.com>
2024-06-09 00:58:53 +0400

    declaration place changed

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,10 +4,9 @@ import sys
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+lisst = []
 try:
     load_from_json_file("add_item.json")
-    lisst = []
     i = 0
     while i < len(sys.argv):
         lisst.append(sys.argv[i])
