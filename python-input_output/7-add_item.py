9bf45aeb25e27d8158164000ca36e71abc7f0cf5
jamesbondie <jamesalfreddie@gmail.com>
2024-06-09 00:57:55 +0400

    filenotfound error handled

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,10 +4,14 @@ import sys
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-load_from_json_file("add_item.json")
-lisst = []
-i = 0
-while i < len(sys.argv):
-    lisst.append(sys.argv[i])
-    i += 1
+
+try:
+    load_from_json_file("add_item.json")
+    lisst = []
+    i = 0
+    while i < len(sys.argv):
+        lisst.append(sys.argv[i])
+        i += 1
+except Exception:
+    pass
 save_to_json_file(lisst, "add_item.json")
