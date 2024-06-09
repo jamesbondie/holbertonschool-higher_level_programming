eca264c56b6848975dd3f147984c980df172e7b9
jamesbondie <jamesalfreddie@gmail.com>
2024-06-09 08:59:47 +0400

    multiple execution without file at the beginning and with only one item added each time

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 lisst = []
 try:
-    load_from_json_file("add_item.json")
+    x = load_from_json_file("add_item.json")
+    for i in x:
+        lisst.append(i)
 except Exception:
     pass
 i = 1
