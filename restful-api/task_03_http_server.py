3589d90ea3be23a4781cfabdddb2cb4de1928b62
unknown <jamesalfreddie@gmail.com>
2024-06-09 15:33:54 +0400

    file created

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,35 @@
+import http.server
+import json
+
+class Subclass(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes("Hello, this is a simple API!", "utf-8"))
+        elif self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes("OK", "utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes("Endpoint not found", "utf-8"))
+
+def run():
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, Subclass)
+    print('Starting server...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
