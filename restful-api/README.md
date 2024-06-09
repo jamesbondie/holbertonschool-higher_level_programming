214926cac7f315d1f2d2ec3f854785b92600f34e
unknown <jamesalfreddie@gmail.com>
2024-06-09 14:28:31 +0400

    csv.dictwriter

---
+++ restful-api/README.md
@@ -0,0 +1,1 @@
+This is the directory for curl, flask, http all about restful
---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,26 @@
+import requests
+import csv
+
+url = 'https://jsonplaceholder.typicode.com/posts'
+requ = requests.get(url)
+
+
+def fetch_and_print_posts():
+    print(requ.status_code)
+    for i in requ.json():
+        print(i['title'])
+
+def fetch_and_save_posts():
+    print(requ.status_code)
+    with open('posts.csv', 'w', newline='') as csvfile:
+        fieldnames = ['id', 'title', 'body']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for date in requ.json():
+            writer.writerow({'id': date['id'],
+                            'title': date['title'],
+                            'body': date['id']})
+
+
+fetch_and_save_posts()
