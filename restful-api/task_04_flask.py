821063159795658e6fcf2a95dc77b3e831b39d5b
jamesbondie <jamesalfreddie@gmail.com>
2024-06-13 18:35:38 +0400

    ""

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 users = {}
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Welcome to the Flask API"
 
 
-@app.route('/data')
+@app.route("/data")
 def data():
     x = []
     for i in users.keys():
@@ -20,12 +20,12 @@ def data():
     return jsonify(x)
 
 
-@app.route('/status')
+@app.route("/status")
 def status():
-    return 'OK'
+    return "OK"
 
 
-@app.route('/users/<username>')
+@app.route("/users/<username>")
 def users(username):
     for i in users.keys():
         if i == username:
@@ -34,7 +34,7 @@ def users(username):
         return jsonify({"error": "User not found"})
 
 
-@app.route('/add_user', methods=['GET', 'POST'])
+@app.route("/add_user", methods=['GET', 'POST'])
 def add_user():
     data = request.json
     diction = {}
