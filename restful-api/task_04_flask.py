4d95d27d0c881d1542ced10d5f4f041820cd66d5
jamesbondie <jamesalfreddie@gmail.com>
2024-06-13 20:12:26 +0400

    fail task recovered

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -39,7 +39,7 @@ def add_user():
     data = request.json
     if data is None:
         return jsonify({"error": "Data not provided"}), 400
-    if 'username' not in data:
+    if data['username'] is None:
         return jsonify({"error": "Username not provided"}), 400
     username = data['username']
     diction = {
