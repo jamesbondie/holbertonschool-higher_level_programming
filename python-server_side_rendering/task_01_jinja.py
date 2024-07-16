413b9543a0046a7d9521d6a41986f21502b7236c
jamesbondie <jamesalfreddie@gmail.com>
2024-07-17 01:01:17 +0400

    route changed

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -7,11 +7,11 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/contact.html')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
