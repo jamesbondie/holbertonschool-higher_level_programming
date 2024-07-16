2b9d1467ea3d3708a721b405e162df15ccf783c7
jamesbondie <jamesalfreddie@gmail.com>
2024-07-17 00:22:17 +0400

    {% include "file.html" %}

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
