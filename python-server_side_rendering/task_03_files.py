cb6b65c7e3ebf2c4f436160f44fc5aa812454d11
jamesbondie <jamesalfreddie@gmail.com>
2024-07-17 23:01:57 +0400

    app runned

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -27,3 +27,8 @@ def sources():
         products = products
 
     return render_template('product_display.html', products=products)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
